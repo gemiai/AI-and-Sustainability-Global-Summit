9f9cfcef63b475019a26f90e951c7188776011f9
“gemiai” <gemiaitech@gmail.com>
2025-06-20 18:43:56 -0400

    feat: Fix styling, restore loading screen, and refactor data handling

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'; // Make sure it's importing from App.js
+import AppWrapper from './App'; // Import the wrapper
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> 
+    <AppWrapper /> 
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
